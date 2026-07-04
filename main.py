0ecce20fe471a3c73d4224fcd713cd24ba258837
Nikola Neychev <nikneichev@abv.bg>
2026-07-04 11:23:06 +0300

    towers added in main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
 import sys
 from settings import SCREEN_WIDTH, SCREEN_HEIGHT, FPS
-from map import GameMap
+from settings import GAME_WIDTH, TOWER_TYPES
+from map import GameMap, pixel_to_cell
 from enemy import Enemy, EnemyGroup
+from tower import Tower, TowerGroup
+from economy import Economy
 
 def main():
     pygame.init()
@@ -12,6 +15,8 @@ def main():
 
     game_map = GameMap(0)
     enemy_group = EnemyGroup()
+    tower_group = TowerGroup()
+    economy = Economy()
 
     enemy_group.add(Enemy('basic', game_map.pixel_waypoints))
 
@@ -22,9 +27,22 @@ def main():
                 pygame.quit()
                 sys.exit()
 
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mx, my = event.pos
+                if mx < GAME_WIDTH:
+                    col, row = pixel_to_cell(mx, my)
+                    if game_map.is_buildable(col, row):
+                        cost = TOWER_TYPES['basic']['cost']
+                        if economy.spend(cost):
+                            t = Tower('basic', col, row)
+                            game_map.place_tower(col, row, t)
+                            tower_group.add(t)
+
+
         enemy_group.update(dt)
         screen.fill((0,0,0))
         game_map.draw(screen)
+        tower_group.draw(screen)
         enemy_group.draw(screen)
         pygame.display.flip()
 
