a7ac64f49b1ab99a4546124e9ac0f077c5b5be02
Nikola Neychev <nikneichev@abv.bg>
2026-07-04 17:42:59 +0300

    lives added in economy.py

--- economy.py
+++ economy.py
@@ -1,8 +1,10 @@
-from settings import STARTING_GOLD
+from settings import STARTING_GOLD, STARTING_LIVES
 
 class Economy:
     def __init__(self):
         self.gold = STARTING_GOLD
+        self.lives = STARTING_LIVES
+        self.score = 0
 
     def can_afford(self, amount):
         return self.gold >= amount
@@ -15,3 +17,10 @@ class Economy:
 
     def earn(self, amount):
         self.gold += amount
+        self.score += amount
+
+    def lose_life(self, count=1):
+        self.lives -= count
+
+    def is_game_ofer(self):
+        return self.lives <= 0
