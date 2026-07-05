a3be09a5b12aa581036cb958f6cebb8f21fd8f89
Nikola Neychev <nikneichev@abv.bg>
2026-07-05 09:14:59 +0300

    enemy.py updated

--- enemy.py
+++ enemy.py
@@ -3,15 +3,20 @@ import math
 from settings import ENEMY_TYPES, DARK_GREY
 
 class Enemy:
-    def __init__(self, enemy_type, pixel_waypoints):
+    def __init__(self, enemy_type, pixel_waypoints, hp_multiplier=1.0, speed_multiplier=1.0):
         data = ENEMY_TYPES[enemy_type]
         self.type = enemy_type
         self.color = data['color']
         self.size = data['size']
-        self.speed = data['speed']
-        self.max_hp = data['hp']
+        self.base_speed = data['speed'] * speed_multiplier
+        self.speed = self.base_speed
+        self.max_hp = int(data['hp'] * hp_multiplier)
         self.hp = self.max_hp
         self.reward = data['reward']
+        self.armor = data.get('armor', 0)
+        self.regen_rate = data.get('regen', 0)
+        self.splits = data.get('splits', False)
+        self.is_ghost = data.get('ghost', False)
 
         self.waypoints = pixel_waypoints
         self.waypoint_index = 1
@@ -23,11 +28,15 @@ class Enemy:
         self.reached_end = False
 
     def take_damage(self, amount):
-        self.hp -= amount
+        actual = max(1, amount - self.armor)
+        self.hp -= actual
         if self.hp <= 0:
             self.alive = False
 
     def update(self, dt):
+        if self.regen_rate > 0 and self.hp < self.max_hp:
+            self.hp = min(self.max_hp, self.hp + self.regen_rate *dt)
+
         if self.waypoint_index >= len(self.waypoints):
             self.reached_end = True
             self.alive = False
@@ -50,8 +59,20 @@ class Enemy:
 
     def draw(self, surface):
         ix, iy = int(self.x), int(self.y)
-        pygame.draw.circle(surface, self.color, (ix, iy), self.size)
-        pygame.draw.circle(surface, DARK_GREY, (ix, iy), self.size, 2)
+        
+        if self.is_ghost:
+            s = pygame.Surface((self.size * 2 + 4, self.size * 2 + 4), pygame.SRCALPHA)
+            pygame.draw.circle(s, (*self.color, 140), (self.size + 2, self.size + 2), self.size)
+            surface.blit(s, (ix - self.size -2, iy - self.size - 2))
+        else:
+            pygame.draw.circle(surface, self.color, (ix, iy), self.size)
+            pygame.draw.circle(surface, DARK_GREY, (ix, iy), self.size, 2)
+
+        if self.armor > 0:
+            pygame.draw.circle(surface, (180, 180, 180), (ix, iy), self.size, 3)
+
+        if self.regen_rate > 0 and self.hp < self.max_hp:
+            pygame.draw.circle(surface, (60, 220, 80), (ix, iy), self.size + 2, 2)
 
         bar_w = self.size * 2
         bar_x = ix - self.size
@@ -76,17 +97,29 @@ class EnemyGroup:
         gold_earned = 0
         alive = []
         lives_lost = 0
+        killed = []
+        spawns = []
+
         for e in self.enemies:
             if not e.alive:
                 if e.reached_end:
                     lives_lost += 1
                 else:
                     gold_earned += e.reward
+                    killed.append(e)
+                    if e.splits:
+                        for _ in range(2):
+                            mini = Enemy('mini', e.waypoints)
+                            mini.x = e.x
+                            mini.y = e.y
+                            mini.waypoint_index = e.waypoint_index
+                            mini.progress = e.progress
+                            spawns.append(mini)
             else:
                 alive.append(e)
             
-        self.enemies = alive
-        return gold_earned, lives_lost
+        self.enemies = alive + spawns
+        return gold_earned, lives_lost, killed
 
     def draw(self, surface):
         for e in self.enemies:
