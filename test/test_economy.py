a60d4aaa2571b06fe999876c05b6dce43ba38554
Nikola Neychev <nikneichev@abv.bg>
2026-07-05 10:12:08 +0300

    can afford tests added

--- test/test_economy.py
+++ test/test_economy.py
@@ -5,6 +5,7 @@ class TestEconomy(unittest.TestCase):
     def setUp(self):
         self.eco = Economy()
 
+    # starting values
     def test_starting_gold(self):
         self.assertEqual(self.eco.gold, 300)
 
@@ -13,6 +14,22 @@ class TestEconomy(unittest.TestCase):
 
     def test_starting_score(self):
         self.assertEqual(self.eco.score, 0)
+
+    
+    # can afford
+    def test_can_afford_exact_amount(self):
+        self.assertTrue(self.eco.can_afford(300))
+
+    def test_can_afford_less_than_gold(self):
+        self.assertTrue(self.eco.can_afford(100))
+
+    def test_cannot_afford_more_than_gold(self):
+        self.assertFalse(self.eco.can_afford(301))
+
+    def test_cannot_afford_zero_gold(self):
+        self.eco.spend(300)
+        self.assertFalse(self.eco.can_afford(1))
+
 
 
 if __name__ == '__main__':
